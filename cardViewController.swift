d66770ce9e3e1c65a1d4efddfa8ab5294e62cabd
mrlefort <mrlefort@gmail.com>
2017-05-17 12:28:27 +0200

    nu virker tilføj kort

--- cardViewController.swift
+++ cardViewController.swift
@@ -13,113 +13,121 @@ class cardViewController: UIViewController{
 
 
 
-    let cardParams = STPCardParams()
+//    let cardParams = STPCardParams()
 
-
+    var paymentField: STPPaymentCardTextField? = nil
+    var tokens =  [String: String]()
     @IBOutlet weak var createCardButton: UIButton!
     
     @IBAction func createCardButton(_ sender: Any) {
-        createToken(cardParams: cardParams)
+        createToken(cardParams: (self.paymentField?.cardParams)!)
     }
 
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        createCardButton.isEnabled = false
-        let paymentField = STPPaymentCardTextField(frame: CGRect(x: 10, y: 10, width:300, height: 44))
-        paymentField.delegate = self as? STPPaymentCardTextFieldDelegate
-        self.view.addSubview(paymentField)
         
-
+        paymentField = STPPaymentCardTextField(frame: CGRect(x: 10, y: 100, width:300, height: 44))
+        paymentField?.delegate = self as? STPPaymentCardTextFieldDelegate
+        self.view.addSubview(paymentField!)
+        
     }
     
     // MARK: STPPaymentCardTextFieldDelegate
     
     func paymentCardTextFieldDidChange(_ textField: STPPaymentCardTextField) {
-        
-        cardParams.number = textField.cardParams.number
-        cardParams.expMonth = textField.cardParams.expMonth
-        cardParams.expYear = textField.cardParams.expYear
-        cardParams.cvc = textField.cardParams.cvc
-        
-        self.createCardButton.isEnabled = textField.isValid
+
+//        self.createCardButton.isEnabled = textField.isValid
     }
   
-    
-    func createToken(cardParams: STPCardParams){
 
-        STPAPIClient.shared().createToken(withCard: cardParams) { (token, error) in
-            if error != nil {
-                // show the error to the user
-            } else if let token = token {
-                self.submitTokenToBackend(token: token){completion in
-                    if completion {
-                        // show a receipt page
-                    } else {
-                        // show the error to the user
-                    }
-                }
-            }
-        }
+    func alert(message: String, title: String = "") {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "OK", style: .default, handler: goBack)
+        alertController.addAction(OKAction)
+        self.present(alertController, animated: true, completion: nil)
     }
     
     
     
-    
-    func submitTokenToBackend(token: STPToken, callback: @escaping (_ completion: Bool)-> ()) {
-            getTokensFromDB(){ dbTokens in
-                
-                let accessToken = dbTokens["accessToken"]!
-                let refreshToken = dbTokens["refreshToken"]!
-
-                let urlPath = "\(baseApiUrl)/users/user/\(LoginViewController.user.email!)"
-                let url = NSURL(string: urlPath)
-                let session = URLSession.shared
-                let request = NSMutableURLRequest(url: url! as URL)
-                request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-                request.addValue(accessToken, forHTTPHeaderField: "accessToken")
-                request.addValue(refreshToken, forHTTPHeaderField: "refreshToken")
-                request.httpMethod = "PUT"
-
-                let jsonObject: [String: Any] = [
-                    "token" : token.tokenId
-                ]
-                
-                do {
-                    let jsonData = try JSONSerialization.data(withJSONObject: jsonObject, options: .prettyPrinted)             // do something with data
-                    // if the call fails, the catch block is executed
-                    
-                    request.httpBody = jsonData
-                    
-                    let task = session.dataTask(with: request as URLRequest, completionHandler: {token, response, error -> Void in
-                        
-                        let httpResponse = response as? HTTPURLResponse;
-                        
-                        if(httpResponse != nil)
-                        {
-                            if(httpResponse!.statusCode == 200)
-                            {
-                                callback(true)
+    func createToken(cardParams: STPCardParams){
+        
+            STPAPIClient.shared().createToken(withCard: cardParams) { (token, error) in
+                if error != nil {
+                    // show the error to the user
+                } else if let token = token {
+            
+                    self.submitTokenToBackend(token: token){completion in
+                        if completion {
+                            // show a receipt page
+                            DispatchQueue.main.async {
+                            self.alert(message: "Du har nu tilføjet et kort til din profil. Nu har du mulighed for at blive Premium kunde.", title: "Kort tilføjet")
+                                
                             }
-                            else
-                            {
-                                callback(false)
+                            
+                        } else {
+                            // show the error to the user
+                            DispatchQueue.main.async {
+                            self.alert(message: "Der skete desværre en fejl. Prøv igen.", title: "Error")
                             }
                         }
-                        else
-                        {
-                            callback(false)
-                        }
-                        
-                    })
-                    task.resume()
-                } catch {
-                    callback(false)
+                    }
+                    
                 }
             }
         
+        
     }
+    
+    
+    func goBack(alert: UIAlertAction) {
+        _ = navigationController?.popViewController(animated: true)
+    }
+    
+    func submitTokenToBackend(token: STPToken, callback: @escaping (_ completion: Bool)-> ()) {
+        
+        getTokensFromDB(){ dbTokens in
+            self.tokens = dbTokens
+        }
+        
+        let urlPath = "\(baseApiUrl)/users/addCardToUser"
+        let url = NSURL(string: urlPath)
+        let session = URLSession.shared
+        let request = NSMutableURLRequest(url: url! as URL)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue(tokens["accessToken"]!, forHTTPHeaderField: "accessToken")
+        request.addValue(tokens["refreshToken"]!, forHTTPHeaderField: "refreshToken")
+        request.httpMethod = "POST"
+        
+        let jsonObject: [String: Any] = [
+            "token" : token.tokenId
+        ]
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: jsonObject, options: .prettyPrinted)             // do something with data
+            
+            request.httpBody = jsonData
+            let task = session.dataTask(with: request as URLRequest, completionHandler: {data, response, error -> Void in
+                let httpResponse = response as? HTTPURLResponse;
+                if(httpResponse!.statusCode == 200)
+                {
+                    callback(true)
+                }
+                else
+                {
+                    
+                    callback(false);
+                }
+            })
+            task.resume()
+        } catch {
+            callback(false)
+        }
+        
+        
+    }
+    
+    
     
     
     
